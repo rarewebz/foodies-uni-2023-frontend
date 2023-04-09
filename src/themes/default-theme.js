43e15f0c225e5b745a6cc082db280fdc59f0a9db
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-04-10 00:04:37 +0530

    set theme

---
+++ src/themes/default-theme.js
@@ -0,0 +1,50 @@
+import {createTheme} from "@mui/material";
+
+/*
+colors
+------
+main-green: #83a31a
+main-yellow: #FFDC2A
+secondary-green: #b9db2f
+
+*/
+
+const DefaultTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#83a31a',
+        },
+        secondary: {
+            main: '#FFDC2A',
+        },
+        btnColor: {
+            white: '#b9db2f',
+            black: '#83a31a',
+        },
+        subText: {
+            main: '#b9db2f'
+        }
+    },
+    typography: {
+        "fontFamily": `"Montserrat", "Helvetica", "Arial", sans-serif`,
+        "fontSize": 14,
+        "fontWeightLight": 300,
+        "fontWeightRegular": 400,
+        "fontWeightMedium": 500,
+        button: {
+            textTransform: 'none'
+        },
+        AppBar: {}
+    },
+    components: {
+        MuiTextField: {
+            styleOverrides: {
+                colorPrimary: {
+                    backgroundColor: colors.secondary,
+                }
+            }
+        }
+    }
+});
+
+export default DefaultTheme;
